93f0685cd77a7b5f5394f91f6a5d64944d0f2669
Steven Schwab <schwabs@umich.edu>
2022-10-23 22:02:02 -0500

    Initial Commit

---
+++ H4X0R News Client/H4X0R_News_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_ClientApp.swift
+//  H4X0R News Client
+//
+//  Created by Steven Schwab on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
